6624d7a5a7c11fac636171b3beda3296921e0b25
Yan Cao <cyandterry@hotmail.com>
2014-08-02 19:58:39 -0400

    Added a log functions

--- like_my_gf.py
+++ like_my_gf.py
@@ -1,69 +1,83 @@
 #!/usr/bin/env python
 from instagram.client import InstagramAPI
 from ConfigParser import SafeConfigParser
+import argparse
 import requests
-import time
+import datetime
 import logging
 
 config_parser = SafeConfigParser()
 config_parser.read('config')
 
-CODE = '57376554e9e8452e82538816e2629354'
-#ACCESS_TOKEN = '531383149.663798b.d227d576320843839b577dd9eda5daa4'
-ACCESS_TOKEN = 'None'
-CONFIG = dict(   client_id     = config_parser.get('Client', 'client_id'),
-                 client_secret = config_parser.get('Client', 'client_secret'),
-                 redirect_uri  = config_parser.get('Client', 'redirect_uri'),
+arg_parser = argparse.ArgumentParser(description='Auto-like my GF\'s Instagram')
+arg_parser.add_argument('-v', '--verbose', action='store_true', help='Increase output verbosity')
+args = arg_parser.parse_args()
+
+CONFIG = dict( client_id     = config_parser.get('Client', 'client_id'),
+               client_secret = config_parser.get('Client', 'client_secret'),
+               redirect_uri  = config_parser.get('Client', 'redirect_uri'),
                  )
-Other = dict( scope        = ['likes'],
+OTHER = dict( scope        = ['likes'],
               access_token = config_parser.get('Access Token', 'access_token'),
               target       = config_parser.get('Target', 'username'),
+              log_path     = config_parser.get('Path', 'log_path')+'like_my_gf.log',
     )
 
-#unauth_api.user_search(target_username, 1)[0].id
-MY_ID = 531383149
-PMPM_ID = 231677378
-print type(config_parser.get('Access Token', 'access_token'))
+if args.verbose:
+    logging.basicConfig(filename=OTHER['log_path'], level=logging.DEBUG)
+else:
+    logging.basicConfig(filename=OTHER['log_path'], level=logging.INFO)
+
 
 def get_auth():
     unauth_api = InstagramAPI(**CONFIG)
-    redirect_uri = unauth_api.get_authorize_login_url(scope = Other['scope'])
+    redirect_uri = unauth_api.get_authorize_login_url(scope = OTHER['scope'])
+
     print "Visit this page and authorize access in your browser:\n", redirect_uri
-    Other['access_token'] = raw_input('Please type in the access code you got\n')
+    code = raw_input('Please type in the access code you got\n')
+    OTHER['access_token'], me = unauth_api.exchange_code_for_access_token(code)
+    print me
     with open('config', 'w') as cf:
-        config_parser.set('Access Token', 'access_token', Other['access_token'])
+        config_parser.set('Access Token', 'access_token', OTHER['access_token'])
         config_parser.write(cf)
 
-
 def auth_request():
-    #access_token, user_info = unauthenticated_api.exchange_code_for_access_token(CODE)
-    api = InstagramAPI(access_token=ACCESS_TOKEN)
-    recent_media, url = api.user_recent_media(user_id=PMPM_ID, count = 20)
-    for media in recent_media:
-        print 'Processing media %s' % media.id
-        liked_user = []
-        users = api.media_likes(media.id)
-        for user in users:
-            liked_user.append(user.username)
-        if 'cyandterry' not in liked_user:
-            print 'Liking media %s' % media.id
-            api.like_media(media.id)
-        else:
-            print 'Already liked media %s, aborting like' % media.id
+    api = InstagramAPI(access_token=OTHER['access_token'])
+    target_ids = api.user_search(OTHER['target'], 1)
 
+    if len(target_ids) > 1:
+        logging.error('Found mutiple users, please check username')
+        return
+
+    target_id = target_ids[0].id
+    my_name   = api.user().username
+    logging.debug('Starting check recent media')
+    recent_media, url = api.user_recent_media(user_id=target_id, count = 20)
+    liked_media = []
+    for media in recent_media:
+        logging.debug('Processing media %s' % media.id)
+        users = api.media_likes(media.id)
+        will_like = True
+        for user in users:
+            if user.username == my_name:
+                will_like = False
+                break
+        if will_like:
+            logging.debug('Liking media %s' % media.id)
+            api.like_media(media.id)
+            liked_media.append(media)
+        else:
+            logging.debug('Already liked media %s, aborting like' % media.id)
+
+    return liked_media
 
 if __name__ == '__main__':
-    if Other['access_token'] == 'None':           # Not mistake, but default token is 'None' but not None
+    logging.info('-'*10+str(datetime.datetime.now())+'-'*10)
+    if OTHER['access_token'] == 'None':           # Not mistake, but default token is 'None' but not None
         get_auth()
-
-    #auth_request()
-
+    liked_media = auth_request()
+    logging.info('-'*10+'Liked '+str(len(liked_media))+' medias'+'-'*10)
 # TODO
-# 1. Make Pagination work
-# 2. Move out the config file and create .in file, register another app since this one is exposed to github
-# 3. Regenerate access code and write it to config file. If access code invalid, go and get new one
+# 2. Msince this one is exposed to github
 # 4. After a successful like, send email with standard resolution photo.
 # 5. Deploy this whole thing to web server.
-
-# Little thing:
-# 1. get user_id by username
